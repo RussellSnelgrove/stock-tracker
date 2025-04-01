e9113cb054ffb168b4802586143e3d638eeaf63f
Russell <22Russell22@gmail.com>
2025-03-31 21:22:53 -0600

    task - add start date parameter by default

--- src/app/pages/Stock.jsx
+++ src/app/pages/Stock.jsx
@@ -15,15 +15,25 @@ function Stocks() {
     useEffect(() => {
         if (!symbol) return;
         setLoading(true);
+        setError(null);
         async function fetchStockData() {
+            const today = new Date();
+            const pastDate = new Date(today);
+            pastDate.setDate(today.getDate() - 365);
+            const startDate = pastDate.toLocaleDateString('en-CA');
             try {
-                const res = await fetch(`/api/stocks/symbol?ticker=${symbol}`);
+                const res = await fetch(`/api/stocks/symbol?ticker=${symbol}&start=${startDate}`);
+                if (!res.ok) {
+                    throw new Error(`Failed to fetch data: ${res.statusText}`);
+                }
                 const result = await res.json();
                 setStockInformation(result);
                 setLoading(false);
             } catch (err) {
                 console.error("Error fetching stock data:", err);
-                setError(err)
+                setError(err.message)
+            } finally {
+                setLoading(false);
             }
 
         }
@@ -31,7 +41,7 @@ function Stocks() {
     }, [symbol]);
 
     if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
+    if (error) return <h1>Error Trying to get Ticker: {symbol}</h1>;
     return (
         <>
             <h1>{stockInformation.meta.longName}</h1>
